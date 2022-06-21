223e59e2e83fa1d296ac87cdae3e4b652847d144
mbHng <buiminh2000@gmx.de>
2022-06-21 12:26:33 +0200

    neu

--- src/views/Termin.vue
+++ src/views/Termin.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import '@fullcalendar/core/vdom'
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
@@ -7,9 +7,36 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list'
 import interactionPlugin from '@fullcalendar/interaction'
 
+const id = ref(10)
 const options = reactive({
   plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
-  initialView: 'dayGridMonth'
+  initialView: 'dayGridMonth',
+  headerToolbar: {
+    left: 'prev, next today',
+    center: 'title',
+    right: 'dayGridMonth, dayGridWeek, listDay'
+  },
+  editable: true,
+  selectable: true,
+  weekends: true,
+  select: (arg) => {
+    id.value = id.value + 1
+
+    const cal = arg.view.calendar
+    cal.unselect()
+    cal.addEvent({
+      // eslint-disable-next-line no-template-curly-in-string
+      id: '${id.value}',
+      // eslint-disable-next-line no-template-curly-in-string
+      title: 'New event ${id.value}',
+      start: arg.start,
+      end: arg.end,
+      allDay: true
+    })
+  },
+  eventClick: (arg) => {
+    console.log(arg.start + arg.end)
+  }
 })
 </script>
 
